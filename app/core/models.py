0995093f200ed744f60336fc77650fcb2b7502ba
Ashwani <ashwani.uuid@gmail.com>
2021-06-05 22:33:35 +0530

    added user to the potato

--- app/core/models.py
+++ app/core/models.py
@@ -7,10 +7,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fiels):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fiels)
+        if not email:
+            raise ValueError('User must have email address')
+        user = self.model(email=self.normalize_email(email), **extra_fiels)
         user.set_password(password)
         user.save(using=self.db)
 
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
         return user
 
 
